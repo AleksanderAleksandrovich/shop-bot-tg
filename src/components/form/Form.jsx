5cb521d1a5ce5f661bc10290f23b40734b6d51a5
AleksanderAleksandrovich <aleksveles228@gmail.com>
2023-01-10 13:01:50 +0300

    add ProductItem

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,27 +1,72 @@
-import React, { useState } from 'react';
-import './Form.css'
+import React, { useCallback, useEffect, useState } from "react";
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Form.css";
 
 const Form = () => {
-    const [country, setCountry] = useState('')
-    const [city, setCity] = useState('')
-    const [object, setObject] = useState('phisical')
+    const [country, setCountry] = useState("");
+    const [city, setCity] = useState("");
+    const [object, setObject] = useState("phisical");
+    const { tg } = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: "Отправить запрос.",
+        });
+    }, []);
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            city,
+            object,
+        }
+        tg.sendData(JSON.stringify(data))
+    },[country, city, object]);
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData);
+
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData);
+        };
+    }, [onSendData]);
+
+    useEffect(() => {
+        if (!city || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, city]);
 
     const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
+        setCountry(e.target.value);
+    };
     const onChangeCity = (e) => {
-        setCity(e.target.value)
-    }
+        setCity(e.target.value);
+    };
     const onChangeObject = (e) => {
-        setObject(e.target.value)
-    }
+        setObject(e.target.value);
+    };
     return (
-        <div className={'form'}>
+        <div className={"form"}>
             <h3>Введите ваши данные</h3>
-            <input className={'input'} type="text" placeholder={'Страна'} value={country} onChange={onChangeCountry}/>
-            <input className={'input'} type="text" placeholder={'Город'}value={city} onChange={onChangeCity}/>
-       
-            <select className='select' value={object} onChange={onChangeObject}>
+            <input
+                className={"input"}
+                type="text"
+                placeholder={"Страна"}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={"input"}
+                type="text"
+                placeholder={"Город"}
+                value={city}
+                onChange={onChangeCity}
+            />
+
+            <select className="select" value={object} onChange={onChangeObject}>
                 <option value="phisikal">Физическое лицо</option>
                 <option value="legal">Юридическое лицо</option>
             </select>
--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,10 +1,118 @@
-import React from 'react';
-import './ProductList.css'
+import React, { useCallback, useEffect, useState } from "react";
+import "./ProductList.css";
+import ProductItem from "../productItem/ProductItem";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const products = [
+    {
+        id: "1",
+        title: "Джинсы",
+        price: 5000,
+        description: "Синего цвета, прямые",
+    },
+    {
+        id: "2",
+        title: "Куртка",
+        price: 12000,
+        description: "Зеленого цвета, теплая",
+    },
+    {
+        id: "3",
+        title: "Джинсы 2",
+        price: 5000,
+        description: "Синего цвета, прямые",
+    },
+    {
+        id: "4",
+        title: "Куртка 8",
+        price: 122,
+        description: "Зеленого цвета, теплая",
+    },
+    {
+        id: "5",
+        title: "Джинсы 3",
+        price: 5000,
+        description: "Синего цвета, прямые",
+    },
+    {
+        id: "6",
+        title: "Куртка 7",
+        price: 600,
+        description: "Зеленого цвета, теплая",
+    },
+    {
+        id: "7",
+        title: "Джинсы 4",
+        price: 5500,
+        description: "Синего цвета, прямые",
+    },
+    {
+        id: "8",
+        title: "Куртка 5",
+        price: 12000,
+        description: "Зеленого цвета, теплая",
+    },
+];
 
 const ProductList = () => {
+    const getTotalPrice = (items) => {
+        return items.reduce((acc, item) => {
+            return (acc += item.price);
+        }, 0);
+    };
+    const [addedItem, setAddedItem] = useState([]);
+
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItem,
+            totalPrice: getTotalPrice(addedItem),
+            queryId,
+        };
+        fetch("https://192.168.0.153:8000/web-data", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+    }, [addedItem]);
+
+    useEffect(() => {
+        tg.onEvent("mainButtonClicked", onSendData);
+
+        return () => {
+            tg.offEvent("mainButtonClicked", onSendData);
+        };
+    }, [onSendData]);
+
+    
+    const { tg, queryId } = useTelegram();
+    const onAdd = (product) => {
+        let newItems = [];
+        const alredyAdded = addedItem.find((item) => item.id === product.id);
+
+        if (alredyAdded) {
+            newItems = addedItem.filter((item) => item.id !== product.id);
+        } else {
+            newItems = [...addedItem, product];
+        }
+
+        setAddedItem(newItems);
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить за  ${getTotalPrice(newItems)}`,
+            });
+        }
+    };
     return (
-        <div>
-            Продукты
+        <div className={"list"}>
+            {products.map((item) => (
+                <ProductItem product={item} onAdd={onAdd} className={"item"} />
+            ))}
         </div>
     );
 };
